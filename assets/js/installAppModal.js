fe82f9bd98763f3bbf0391d4ffdaf16148f0b747
Ktumsh <jbz.january@gmail.com>
2024-05-24 15:55:39 -0400

    .

--- assets/js/installAppModal.js
+++ assets/js/installAppModal.js
@@ -1,59 +1,60 @@
-let deferredPrompt;
+let installPrompt;
 const isPWA = window.matchMedia("(display-mode: standalone)").matches;
 
 window.addEventListener("beforeinstallprompt", (e) => {
-  deferredPrompt = e;
+  installPrompt = e;
 });
 
 window.addEventListener("appinstalled", () => {
   console.log("PWA instalada");
-  localStorage.setItem("pwaInstalled", "true");
 });
 
-function isMobileDevice() {
-  return /Mobi|Android/i.test(navigator.userAgent);
+function isMobile() {
+  return /Mobi|Android|iPhone|iPad/i.test(navigator.userAgent);
 }
 
 const body = document.body;
-const downloadAppModal = document.getElementById("downloadAppModal");
+const modal = document.getElementById("downloadAppModal");
 const modalCtn = document.querySelector(".modal_position_content");
 
 window.addEventListener("load", () => {
   const modalShown = sessionStorage.getItem("modalShown") === "true";
 
-  if (isMobileDevice() && !modalShown) {
-    downloadAppModal.style.display = "block";
+  if (isPWA) {
+    return;
+  }
+
+  if (isMobile() && !modalShown) {
+    modal.style.display = "block";
     setTimeout(() => {
       body.classList.add("hidden_body");
-      downloadAppModal.classList.add("show");
+      modal.classList.add("show");
       modalCtn.classList.add("show");
     }, 2000);
+
+    const isIOS = /iPhone|iPad/i.test(navigator.userAgent);
+    if (isIOS) {
+      const iosInfo = document.getElementById("iosInfo");
+      iosInfo.style.display = "block";
+    }
+
     sessionStorage.setItem("modalShown", "true");
   }
 });
 
 document.querySelector(".close_button").addEventListener("click", () => {
   body.classList.remove("hidden_body");
-  downloadAppModal.classList.remove("show");
+  modal.classList.remove("show");
   modalCtn.classList.remove("show");
-  downloadAppModal.style.display = "none";
+  modal.style.display = "none";
 });
 
 document.getElementById("installApp").addEventListener("click", () => {
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt");
-        localStorage.setItem("pwaInstalled", "true");
-      } else {
-        console.log("User dismissed the A2HS prompt");
-      }
-      deferredPrompt = null;
-    });
+  if (installPrompt) {
+    installPrompt.prompt();
   } else {
     alert(
-      "Aplicación ya instalada, si crees que se trata de un error, instala la app al final de la página."
+      "Tienes la aplicación instalada, si crees que se trata de un error, instala la app al final de la página."
     );
   }
 });
