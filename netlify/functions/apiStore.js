713a8ea3e9d747328899c29e06f39c3feba6513d
Ktumsh <jbz.january@gmail.com>
2024-05-28 01:47:55 -0400

    .

---
+++ netlify/functions/apiStore.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+exports.handler = async (event, context) => {
+  const filePath = path.join(__dirname, "../../data/api/apiStore.json");
+  try {
+    const data = fs.readFileSync(filePath, "utf8");
+    return {
+      statusCode: 200,
+      body: data,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Error reading the API file" }),
+    };
+  }
+};
