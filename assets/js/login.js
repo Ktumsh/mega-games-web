3d0fb4fb067b7961b68e1a6669c23699668cce41
Ktumsh <jbz.january@gmail.com>
2024-05-30 15:11:55 -0400

    .

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,7 +5,7 @@ document.getElementById("login").addEventListener("submit", async (e) => {
   e.preventDefault();
   const identifier = document.getElementById("username");
   const password = document.getElementById("password");
-  const res = await fetch(`${apiUrl}/api/users/login`, {
+  const res = await fetch(`${apiUrl}/api/login`, {
     method: "POST",
     mode: "cors",
     headers: {
